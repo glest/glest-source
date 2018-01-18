61738cc167ca20fb8f35c0d6dbcbc5e93ce56e69
andy5995 <andy400-dev@yahoo.com>
2018-01-17 21:18:15 -0600

    git_update.sh:fix [skip ci]
    This script is for people who clone the repo without forking
    
    https://github.com/ZetaGlest/zetaglest-source#try-the-game-without-contributing

--- git_update.sh
+++ git_update.sh
@@ -3,14 +3,19 @@ export LANG=C
 
 cd "$(dirname $(readlink -f $0))"
 
-cd git pull &&
+git pull &&
 
 cd data &&
 git pull &&
 
+cd .. &&
+
 cd source/masterserver &&
 git pull &&
 
-cd mk/linux/tools-for-standalone-client/installer/mojosetup
-git pull
+cd ../.. &&
 
+cd mk/linux/tools-for-standalone-client/installer/mojosetup
+git pull &&
+
+exit 0
