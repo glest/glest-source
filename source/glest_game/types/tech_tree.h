b6a891d6c13d5eee345a474c30eba473613f4eee
Andy Alt <andy5995@users.noreply.github.com>
2018-02-03 05:49:20 -0600

    types:reformat/style (#47)
    `indent -i2 -nut -bl -bli0 -ppi3 -nce -npsl` Merging @andy5995 's code reformatting.

--- source/glest_game/types/tech_tree.h
+++ source/glest_game/types/tech_tree.h
@@ -1,119 +1,162 @@
 // ==============================================================
-//	This file is part of Glest (www.glest.org)
+//      This file is part of Glest (www.glest.org)
 //
-//	Copyright (C) 2001-2008 Martiño Figueroa
+//      Copyright (C) 2001-2008 Martiño Figueroa
 //
-//	You can redistribute this code and/or modify it under
-//	the terms of the GNU General Public License as published
-//	by the Free Software Foundation; either version 2 of the
-//	License, or (at your option) any later version
+//      You can redistribute this code and/or modify it under
+//      the terms of the GNU General Public License as published
+//      by the Free Software Foundation; either version 2 of the
+//      License, or (at your option) any later version
 // ==============================================================
 
 #ifndef _GLEST_GAME_TECHTREE_H_
-#define _GLEST_GAME_TECHTREE_H_
+#   define _GLEST_GAME_TECHTREE_H_
 
-#ifdef WIN32
-    #include <winsock2.h>
-    #include <winsock.h>
-#endif
+#   ifdef WIN32
+#      include <winsock2.h>
+#      include <winsock.h>
+#   endif
 
-#include <set>
-#include "util.h"
-#include "resource_type.h"
-#include "faction_type.h"
-#include "damage_multiplier.h"
-#include "leak_dumper.h"
+#   include <set>
+#   include "util.h"
+#   include "resource_type.h"
+#   include "faction_type.h"
+#   include "damage_multiplier.h"
+#   include "leak_dumper.h"
 
-namespace Glest{ namespace Game{
+namespace Glest
+{
+  namespace Game
+  {
 
 // =====================================================
-// 	class TechTree
+//      class TechTree
 //
-///	A set of factions and resources
+///     A set of factions and resources
 // =====================================================
 
-class TechTree{
-private:
-	typedef vector<ResourceType> ResourceTypes;
-	typedef vector<FactionType> FactionTypes;
-	typedef vector<ArmorType> ArmorTypes;
-	typedef vector<AttackType> AttackTypes;
+    class TechTree
+    {
+    private:
+      typedef vector < ResourceType > ResourceTypes;
+      typedef vector < FactionType > FactionTypes;
+      typedef vector < ArmorType > ArmorTypes;
+      typedef vector < AttackType > AttackTypes;
 
-private:
+    private:
 
-	string name;
-    //string desc;
-	string treePath;
-	vector<string> pathList;
+        string name;
+      //string desc;
+      string treePath;
+        vector < string > pathList;
 
-    ResourceTypes resourceTypes;
-    FactionTypes factionTypes;
-	ArmorTypes armorTypes;
-	AttackTypes attackTypes;
-	DamageMultiplierTable damageMultiplierTable;
-	Checksum checksumValue;
+      ResourceTypes resourceTypes;
+      FactionTypes factionTypes;
+      ArmorTypes armorTypes;
+      AttackTypes attackTypes;
+      DamageMultiplierTable damageMultiplierTable;
+      Checksum checksumValue;
 
-	string languageUsedForCache;
-	std::map<string,string> translatedTechNames;
-	std::map<string,std::map<string,string> > translatedTechFactionNames;
-	bool isValidationModeEnabled;
+      string languageUsedForCache;
+        std::map < string, string > translatedTechNames;
+        std::map < string, std::map < string,
+        string > >translatedTechFactionNames;
+      bool isValidationModeEnabled;
 
-public:
-    Checksum loadTech(const string &techName,
-    		set<string> &factions, Checksum* checksum,
-    		std::map<string,vector<pair<string, string> > > &loadedFileList,
-    		bool validationMode=false);
-    void load(const string &dir, set<string> &factions, Checksum* checksum,
-    		Checksum *techtreeChecksum,
-    		std::map<string,vector<pair<string, string> > > &loadedFileList,
-    		bool validationMode=false);
-    string findPath(const string &techName) const;
+    public:
+        Checksum loadTech (const string & techName,
+                           set < string > &factions, Checksum * checksum,
+                           std::map < string, vector < pair < string,
+                           string > > >&loadedFileList, bool validationMode =
+                           false);
+      void load (const string & dir, set < string > &factions,
+                 Checksum * checksum, Checksum * techtreeChecksum,
+                 std::map < string, vector < pair < string,
+                 string > > >&loadedFileList, bool validationMode = false);
+      string findPath (const string & techName) const;
 
-    static string findPath(const string &techName, const vector<string> &pathTechList);
-    static bool exists(const string &techName, const vector<string> &pathTechList);
+      static string findPath (const string & techName,
+                              const vector < string > &pathTechList);
+      static bool exists (const string & techName,
+                          const vector < string > &pathTechList);
 
-    explicit TechTree(const vector<string> pathList);
-    ~TechTree();
-    Checksum * getChecksumValue() { return &checksumValue; }
+      explicit TechTree (const vector < string > pathList);
+       ~TechTree ();
+      Checksum *getChecksumValue ()
+      {
+        return &checksumValue;
+      }
 
-    //get
-	int getResourceTypeCount() const							{return (int)resourceTypes.size();}
-	int getTypeCount() const									{return (int)factionTypes.size();}
-	const FactionType *getType(int i) const						{return &factionTypes[i];}
-	const ResourceType *getResourceType(int i) const			{return &resourceTypes[i];}
-	string getName(bool translatedValue=false);
-	string getNameUntranslated() const;
+      //get
+      int getResourceTypeCount () const
+      {
+        return (int) resourceTypes.size ();
+      }
+      int getTypeCount () const
+      {
+        return (int) factionTypes.size ();
+      }
+      const FactionType *getType (int i) const
+      {
+        return &factionTypes[i];
+      }
+      const ResourceType *getResourceType (int i) const
+      {
+        return &resourceTypes[i];
+      }
+      string getName (bool translatedValue = false);
+      string getNameUntranslated () const;
 
-	string getTranslatedName(string techName, bool forceLoad=false, bool forceTechtreeActiveFile=false);
-	string getTranslatedFactionName(string techName, string factionName);
+      string getTranslatedName (string techName, bool forceLoad =
+                                false, bool forceTechtreeActiveFile = false);
+      string getTranslatedFactionName (string techName, string factionName);
 
-	vector<string> getPathList() const					{return pathList;}
-    //const string &getDesc() const								{return desc;}
+        vector < string > getPathList () const
+      {
+        return pathList;
+      }
+      //const string &getDesc() const                                                             {return desc;}
 
-	const string getPath() const								{return treePath;}
+      const string getPath () const
+      {
+        return treePath;
+      }
 
-	const FactionType *getType(const string &name) const;
-	FactionType *getTypeByName(const string &name);
-	const ResourceType *getResourceType(const string &name) const;
-    const ResourceType *getTechResourceType(int i) const;
-    const ResourceType *getFirstTechResourceType() const;
+      const FactionType *getType (const string & name) const;
+      FactionType *getTypeByName (const string & name);
+      const ResourceType *getResourceType (const string & name) const;
+      const ResourceType *getTechResourceType (int i) const;
+      const ResourceType *getFirstTechResourceType () const;
 
-    const ArmorType *getArmorType(const string &name) const;
-	const AttackType *getAttackType(const string &name) const;
+      const ArmorType *getArmorType (const string & name) const;
+      const AttackType *getAttackType (const string & name) const;
 
-    int getArmorTypeCount() const { return (int)armorTypes.size(); }
-    const ArmorType * getArmorTypeByIndex(int index) const { return &armorTypes[index]; }
-	int getAttackTypeCount() const { return (int)attackTypes.size(); }
-	const AttackType * getAttackTypeByIndex(int index) const { return &attackTypes[index]; }
+      int getArmorTypeCount () const
+      {
+        return (int) armorTypes.size ();
+      }
+      const ArmorType *getArmorTypeByIndex (int index) const
+      {
+        return &armorTypes[index];
+      }
+      int getAttackTypeCount () const
+      {
+        return (int) attackTypes.size ();
+      }
+      const AttackType *getAttackTypeByIndex (int index) const
+      {
+        return &attackTypes[index];
+      }
 
-	double getDamageMultiplier(const AttackType *att, const ArmorType *art) const;
-	std::vector<std::string> validateFactionTypes();
-	std::vector<std::string> validateResourceTypes();
+      double getDamageMultiplier (const AttackType * att,
+                                  const ArmorType * art) const;
+        std::vector < std::string > validateFactionTypes ();
+        std::vector < std::string > validateResourceTypes ();
 
-	void saveGame(XmlNode *rootNode);
+      void saveGame (XmlNode * rootNode);
 
-};
+    };
 
-}} //end namespace
+}}                              //end namespace
 
 #endif
--- source/glest_game/types/tileset_model_type.h
+++ source/glest_game/types/tileset_model_type.h
@@ -1,84 +1,130 @@
 // ==============================================================
-//	This file is part of Glest (www.glest.org)
+//      This file is part of Glest (www.glest.org)
 //
-//	Copyright (C) 2001-2008 Martiño Figueroa
+//      Copyright (C) 2001-2008 Martiño Figueroa
 //
-//	You can redistribute this code and/or modify it under 
-//	the terms of the GNU General Public License as published 
-//	by the Free Software Foundation; either version 2 of the 
-//	License, or (at your option) any later version
+//      You can redistribute this code and/or modify it under 
+//      the terms of the GNU General Public License as published 
+//      by the Free Software Foundation; either version 2 of the 
+//      License, or (at your option) any later version
 // ==============================================================
 #ifndef _GLEST_GAME_TILESET_MODEL_TYPE_H_
-#define _GLEST_GAME_TILESET_MODEL_TYPE_H_
+#   define _GLEST_GAME_TILESET_MODEL_TYPE_H_
 
-#ifdef WIN32
-    #include <winsock2.h>
-    #include <winsock.h>
-#endif
+#   ifdef WIN32
+#      include <winsock2.h>
+#      include <winsock.h>
+#   endif
 
-#include <vector>
-#include "model.h"
-#include "vec.h"
-#include "leak_dumper.h"
-#include "unit_particle_type.h"
+#   include <vector>
+#   include "model.h"
+#   include "vec.h"
+#   include "leak_dumper.h"
+#   include "unit_particle_type.h"
 
 using std::vector;
 
-namespace Glest{ namespace Game{
+namespace Glest
+{
+  namespace Game
+  {
 
-using Shared::Graphics::Model;
-using Shared::Graphics::Vec3f;
+    using Shared::Graphics::Model;
+    using Shared::Graphics::Vec3f;
 
 // =====================================================
-// 	class ObjectType  
+//      class ObjectType  
 //
-///	Each of the possible objects of the map: trees, stones ...
+///     Each of the possible objects of the map: trees, stones ...
 // =====================================================
 
-typedef vector<ObjectParticleSystemType*> ModelParticleSystemTypes;
+    typedef vector < ObjectParticleSystemType * >ModelParticleSystemTypes;
 
-class TilesetModelType {
-private:
-	Model *model;
-	ModelParticleSystemTypes particleTypes;
-	int height;
-	bool rotationAllowed;
-	bool randomPositionEnabled;
-	bool smoothTwoFrameAnim;
+    class TilesetModelType
+    {
+    private:
+      Model * model;
+      ModelParticleSystemTypes particleTypes;
+      int height;
+      bool rotationAllowed;
+      bool randomPositionEnabled;
+      bool smoothTwoFrameAnim;
 
-    int animSpeed;
+      int animSpeed;
 
-public:
-	TilesetModelType();
-	~TilesetModelType();
+    public:
+      TilesetModelType ();
+      ~TilesetModelType ();
 
-	void addParticleSystem(ObjectParticleSystemType *particleSystem);
-	inline bool hasParticles()	const		{return particleTypes.empty() == false;}
-	inline ModelParticleSystemTypes* getParticleTypes()  { return &particleTypes ;}
+      void addParticleSystem (ObjectParticleSystemType * particleSystem);
+      inline bool hasParticles () const
+      {
+        return particleTypes.empty () == false;
+      }
+      inline ModelParticleSystemTypes *getParticleTypes ()
+      {
+        return &particleTypes;
+      }
 
 
-	inline Model * getModel() const		{return model;}
-	inline void setModel(Model *model) 	{this->model=model;}
+      inline Model *getModel () const
+      {
+        return model;
+      }
+      inline void setModel (Model * model)
+      {
+        this->model = model;
+      }
 
-	inline int getHeight() const			{return height;}
-	inline void setHeight(int height) 			{this->height=height;}
+      inline int getHeight () const
+      {
+        return height;
+      }
+      inline void setHeight (int height)
+      {
+        this->height = height;
+      }
 
-	inline bool getRotationAllowed() const			{return rotationAllowed;}
-	inline void setRotationAllowed(bool rotationAllowed)	{this->rotationAllowed=rotationAllowed;}
-	
-	inline bool getRandomPositionEnabled() const			{return randomPositionEnabled;}
-	inline void setRandomPositionEnabled(bool randomPositionEnabled)	{this->randomPositionEnabled=randomPositionEnabled;}
+      inline bool getRotationAllowed () const
+      {
+        return rotationAllowed;
+      }
+      inline void setRotationAllowed (bool rotationAllowed)
+      {
+        this->rotationAllowed = rotationAllowed;
+      }
 
-	inline bool getSmoothTwoFrameAnim() const			{return smoothTwoFrameAnim;}
-	inline void setSmoothTwoFrameAnim(bool smoothTwoFrameAnim)	{this->smoothTwoFrameAnim=smoothTwoFrameAnim;}
+      inline bool getRandomPositionEnabled () const
+      {
+        return randomPositionEnabled;
+      }
+      inline void setRandomPositionEnabled (bool randomPositionEnabled)
+      {
+        this->randomPositionEnabled = randomPositionEnabled;
+      }
 
-	inline int getAnimSpeed() const			{return animSpeed;}
-	inline void setAnimSpeed(int value) {animSpeed = value;}
+      inline bool getSmoothTwoFrameAnim () const
+      {
+        return smoothTwoFrameAnim;
+      }
+      inline void setSmoothTwoFrameAnim (bool smoothTwoFrameAnim)
+      {
+        this->smoothTwoFrameAnim = smoothTwoFrameAnim;
+      }
 
-//	inline int getAnimSpeedVariation() const			{return animVariation;}
-//	inline void setAnimSpeedVariation(int value) {animSpeed = valueVariation;}
-};
+      inline int getAnimSpeed () const
+      {
+        return animSpeed;
+      }
+      inline void setAnimSpeed (int value)
+      {
+        animSpeed = value;
+      }
 
-}}//end namespace
+//      inline int getAnimSpeedVariation() const                        {return animVariation;}
+//      inline void setAnimSpeedVariation(int value) {animSpeed = valueVariation;}
+    };
+
+}}                              //end namespace
 
 #endif
