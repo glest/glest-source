e135baba85d2b2e18ed6f4c753dec065e87b34e3
andy5995 <andy400-dev@yahoo.com>
2018-03-02 14:24:54 -0600

    server_interface:lag:allow a greater grace period before dropping player

--- source/glest_game/network/connection_slot.h
+++ source/glest_game/network/connection_slot.h
@@ -1,13 +1,22 @@
-// ==============================================================
-//	This file is part of Glest (www.glest.org)
+//      connection_slot.h:
+//      connected clients (not the host or user setting up the game)
 //
-//	Copyright (C) 2001-2008 Martiño Figueroa
+//      Copyright (C) 2018  The ZetaGlest team <https://github.com/ZetaGlest>
 //
-//	You can redistribute this code and/or modify it under
-//	the terms of the GNU General Public License as published
-//	by the Free Software Foundation; either version 2 of the
-//	License, or (at your option) any later version
-// ==============================================================
+//      ZetaGlest is a fork of MegaGlest <https://megaglest.org>
+//
+//      This program is free software: you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation, either version 3 of the License, or
+//      (at your option) any later version.
+//
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef _GLEST_GAME_CONNECTIONSLOT_H_
 #define _GLEST_GAME_CONNECTIONSLOT_H_
@@ -145,6 +154,7 @@ private:
 	ConnectionSlotThread* slotThreadWorker;
 	int currentFrameCount;
 	int currentLagCount;
+	int graceLagCtr;
 	time_t lastReceiveCommandListTime;
 	bool gotLagCountWarning;
 	string versionString;
@@ -186,6 +196,9 @@ public:
 
 	bool getSkipLagCheck() const { return skipLagCheck; }
 	bool getJoinGameInProgress() const { return joinGameInProgress; }
+
+	int getGraceLagCtr() { return graceLagCtr++; }
+	void resetGraceLagCtr() { graceLagCtr = 0; }
 
 	bool getSentSavedGameInfo() const { return sentSavedGameInfo; }
 	void setSentSavedGameInfo(bool value) { sentSavedGameInfo = value; }
