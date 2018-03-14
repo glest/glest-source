ff9d772d049a0c97b397a24782c2ff35b3a5c48c
mathusummut <mathusum.mut@gmail.com>
2018-03-15 00:48:18 +0100

    Finalized Windows build system

---
+++ mk/windows/clean-zg.bat
@@ -0,0 +1,64 @@
+@echo off
+setlocal
+echo This will recursively delete all .lib, .exe, .dll, .db, .iobj, .opendb, .pdb, .ipdb, .map, .ncb, .bsc, .cache, .lastbuildstate, .log, .tlog, .exp, .pch and .ipch files in the vs2015 and vs2017 directories.
+SET /P AREYOUSURE=Are you sure you want to do this? (y/[n]) 
+IF /I "%AREYOUSURE%" NEQ "y" GOTO END
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+IF EXIST .\vs2015-32 (
+	cd .\vs2015-32
+	CALL :DELETE
+	del /S *.obj
+	cd ..\
+)
+
+IF EXIST .\vs2015-64 (
+	cd .\vs2015-64
+	CALL :DELETE
+	del /S *.obj
+	cd ..\
+)
+
+IF EXIST .\vs2017-32 (
+	cd .\vs2017-32
+	CALL :DELETE
+	del /S *.obj
+	cd ..\
+)
+
+IF EXIST .\vs2017-64 (
+	cd .\vs2017-64
+	CALL :DELETE
+	del /S *.obj
+	cd ..\
+)
+
+ROBOCOPY . . /S /MOVE
+ROBOCOPY . . /S /MOVE
+ROBOCOPY . . /S /MOVE
+
+GOTO END
+
+:DELETE
+del /S *.lib
+del /S *.exe
+del /S *.dll
+del /S *.db
+del /S *.iobj
+del /S *.opendb
+del /S *.pdb
+del /S *.ipdb
+del /S *.map
+del /S *.ncb
+del /S *.bsc
+del /S *.log
+del /S *.tlog
+del /S *.cache
+del /S *.lastbuildstate
+del /S *.exp
+del /S *.ipch
+del /S *.pch
+EXIT /B
+
+:END
+endlocal
