glest/glest-source

commit 045b0b88e051f7163fbef42fa0adc63a326ca186

Remove MG_ and _NNN from Players List
Remove MG_ and _NNN from Players List but not inside chat, that is for another day.

source/glest_game/menu/menu_state_masterserver.cpp
@@ -1278,7 +1278,13 @@ namespace Glest
             button->setFont (CoreData::getInstance ().getDisplayFontSmall ());
             button->setFont3D (CoreData::getInstance ().
                                getDisplayFontSmall3D ());
-            button->setText (nickList[i]);
+            
+            if (strncmp (&nickList[i][0], "ZG_", 3) == 0) {
+                button->setText (nickList[i].substr(3,nickList[i].length()-7));
+            } else {
+                button->setText (nickList[i]);
+            }
+            
             if (strncmp (&nickList[i][0], "ZG_", 3) != 0
                 || &nickList[i][0] == currentIrcNick)
             {

commit aa46b2a8a1d4800c7fc730b4e829a56c8f2bf013

Fixed bug caused by 045b0b88e051f7163fbef42fa0adc63a326ca186, where user could not click on nick button to get real nick to ping.

source/glest_game/menu/menu_state_masterserver.cpp
@@ -997,11 +997,11 @@ namespace Glest
               if (!chatManager.getEditEnabled ())
               {
                 chatManager.switchOnEdit ();
-                chatManager.addText (userButtons[i]->getText () + " ");
+                chatManager.addText (nickList[i] + " ");
               }
               else
               {
-                chatManager.addText (userButtons[i]->getText ());
+                chatManager.addText (nickList[i]);
               }
               break;
             }
@@ -1240,7 +1240,7 @@ namespace Glest
                                         intToStr (__LINE__));
       if (ircClient != NULL)
       {
-        std::vector < string > nickList = ircClient->getNickList ();
+        nickList = ircClient->getNickList ();
 
         if (currentIrcNick != ircClient->getNick ())
         {
@@ -1278,13 +1278,13 @@ namespace Glest
             button->setFont (CoreData::getInstance ().getDisplayFontSmall ());
             button->setFont3D (CoreData::getInstance ().
                                getDisplayFontSmall3D ());
-            
+
             if (strncmp (&nickList[i][0], "ZG_", 3) == 0) {
                 button->setText (nickList[i].substr(3,nickList[i].length()-7));
             } else {
                 button->setText (nickList[i]);
             }
-            
+
             if (strncmp (&nickList[i][0], "ZG_", 3) != 0
                 || &nickList[i][0] == currentIrcNick)
             {
@@ -1296,6 +1296,7 @@ namespace Glest
             }
 
             userButtons.push_back (button);
+
           }
           userScrollBar.setElementCount ((int) userButtons.size ());
           oldNickList = nickList;

source/glest_game/menu/menu_state_masterserver.h
@@ -170,6 +170,8 @@ namespace Glest
                                       const char **params,
                                       unsigned int count);
 
+      std::vector < string > nickList;
+
     };
 
 