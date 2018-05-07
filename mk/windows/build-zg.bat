56e5904c5dbe9a17e957b0e16692f2475c5f00dc
mathusummut <mathusum.mut@gmail.com>
2018-05-07 23:58:46 +0200

    Added friendly messages for Windows build

--- mk/windows/build-zg.bat
+++ mk/windows/build-zg.bat
@@ -167,4 +167,8 @@ ECHO Copying binaries...
 ECHO.
 call .\group-binaries.bat
 ECHO ... Done.
+ECHO.
+ECHO If build was successful, then output is in bin folder, otherwise scroll up a little to view error messages.
+ECHO Thanks for trying out ZetaGlest!
+ECHO.
 pause
