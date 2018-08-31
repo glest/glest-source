7ed5373650c397db2de2ea1370762835fdad0e0e
andy5995 <andy400-dev@yahoo.com>
2018-08-31 12:50:16 -0500

    travis:allow unauthenticated llvm repo
    the llvm toolchain is needed for the clang-6.0 test.
    
    Recently something changed on travis where adding the llvm toolchain by
    using the "addons" directive in .travis.yml fails because
    "allow-unauthenticated" isn't
    passed, which seems to be the only way to add the repo. Moving that to
    the script, and cleaning up the script a little.

--- .travis-before_install.sh
+++ .travis-before_install.sh
@@ -2,67 +2,67 @@
 # script for use with travis and on linux only
 #
 # Copyright (c) 2015-2016 MegaGlest Team under GNU GPL v3.0+
+#
+# Copyright (c) 2018
+# modified for ZetaGlest <https://github.com/ZetaGlest>
+# ZetaGlest Team
 
-export LANG=C
-SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
-# ----------------------------------------------------------------------------
-# Load shared functions
-. $SCRIPTDIR/mk/linux/mg_shared.sh
-detect_system
-# ----------------------------------------------------------------------------
+# This only needs to be changed when Travis updates the Ubuntu version
+# it's using.
+codename=trusty
+
 Compiler_name="$1"; Compiler_version="$2"
 Compiler_version_grep="$(echo "$Compiler_version" | sed 's/\./\\./g')"
 
 set -x
 if [ "$Compiler_version" != "" ] && [ "$Compiler_version" != "default" ]; then
-    # UPDATE REPOS
-    sudo apt-get update -qq
-    sudo apt-get install -y -qq
+  # UPDATE REPOS
+  sudo apt-get update -qq
+  sudo apt-get install -y -qq
 
-    set +x
+  set +x
+  if [ "$Compiler_name" = "gcc" ]; then
+    VersionAvByDefault="$(apt-cache search ^g[c+][c+]-[0-9] | grep -v '[0-9]-[a-zA-Z]' | grep "^gcc-$Compiler_version_grep")"
+  elif [ "$Compiler_name" = "clang" ]; then
+    VersionAvByDefault="$(apt-cache search ^clang-[0-9] | grep -v '[0-9]-[a-zA-Z]' | grep "^clang-$Compiler_version_grep")"
+  fi
+  set -x
+  if [ "$VersionAvByDefault" = "" ]; then
     if [ "$Compiler_name" = "gcc" ]; then
-	VersionAvByDefault="$(apt-cache search ^g[c+][c+]-[0-9] | grep -v '[0-9]-[a-zA-Z]' | grep "^gcc-$Compiler_version_grep")"
-    elif [ "$Compiler_name" = "clang" ]; then
-	VersionAvByDefault="$(apt-cache search ^clang-[0-9] | grep -v '[0-9]-[a-zA-Z]' | grep "^clang-$Compiler_version_grep")"
+
+      # ubuntu test toolchain needed for more recent version of gcc
+      sudo add-apt-repository --yes "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu ${codename} main"
     fi
-    set -x
-    if [ "$VersionAvByDefault" = "" ]; then
-	if [ "$Compiler_name" = "gcc" ]; then
-		# https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/test
-		sudo add-apt-repository --yes "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu ${codename} main"
-		#sudo add-apt-repository --yes "deb-src http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu ${codename} main"
-	fi
-    fi
+  fi
 fi
 
 if [ "$VersionAvByDefault" = "" ]; then
-    # UPDATE REPOS
-    sudo apt-get update -qq
-    #sudo apt-get upgrade -qq # UPGRADE SYSTEM TO LATEST PATCH LEVEL
-    sudo apt-get install -y -qq
+  # UPDATE REPOS
+  sudo apt-get update -qq
+  sudo apt-get install -y -qq
 fi
 set -e
 
 if [ "$Compiler_version" != "" ] && [ "$Compiler_version" != "default" ]; then
-    if [ "$Compiler_name" = "gcc" ]; then
-	set +ex
-	Gcc_AvSepGpp="$(apt-cache search ^g[c+][c+]-[0-9] | grep -v '[0-9]-[a-zA-Z]' | grep "^g++-$Compiler_version_grep")"
-	set -ex
-	if [ "$Gcc_AvSepGpp" = "" ]; then
-	    sudo apt-get --allow-unauthenticated install -qq --force-yes gcc-${Compiler_version}
-	else
-	    sudo apt-get --allow-unauthenticated install -qq --force-yes gcc-${Compiler_version} g++-${Compiler_version}
-	fi
-    elif [ "$Compiler_name" = "clang" ]; then
-	sudo apt-get --allow-unauthenticated install -qq --force-yes clang-${Compiler_version}
+  if [ "$Compiler_name" = "gcc" ]; then
+    set +ex
+    Gcc_AvSepGpp="$(apt-cache search ^g[c+][c+]-[0-9] | grep -v '[0-9]-[a-zA-Z]' | grep "^g++-$Compiler_version_grep")"
+    set -ex
+    if [ "$Gcc_AvSepGpp" = "" ]; then
+      sudo apt-get --allow-unauthenticated install -qq --force-yes gcc-${Compiler_version}
+    else
+      sudo apt-get --allow-unauthenticated install -qq --force-yes gcc-${Compiler_version} g++-${Compiler_version}
     fi
-fi
+  elif [ "$Compiler_name" = "clang" && ]; then
+      export DEBIAN_FRONTEND=noninteractive
 
-# what available versions we can use
-set +x
-apt-cache search ^g[c+][c+]-[0-9] | grep -v '[0-9]-[a-zA-Z]'
-apt-cache search ^clang-[0-9] | grep -v '[0-9]-[a-zA-Z]'
-set -x
+      # llvm-toolchain needed for more recent version of clang
+      echo "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main" | sudo tee -a /etc/apt/sources.list >/dev/null
+      sudo -E apt-get -yq update &>> ~/apt-get-update.log
+      sudo -E apt-get -yq --allow-unauthenticated --no-install-suggests \
+          --no-install-recommends $TRAVIS_APT_OPTS install clang-${Compiler_version}
+  fi
+fi
 
 # INSTALL OUR DEPENDENCIES
 
