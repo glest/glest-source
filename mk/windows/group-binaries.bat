70bc3b5873459a7e7d7e9ea05520e6ae8f640b3c
mathusummut <mathusum.mut@gmail.com>
2018-09-30 22:19:57 +0200

    Fixed Windows script

--- mk/windows/group-binaries.bat
+++ mk/windows/group-binaries.bat
@@ -16,20 +16,20 @@ copy /y ..\shared\glestkeys.ini bin\vs2015\glestkeys.ini
 copy /y ..\shared\glestkeys.ini bin\vs2017\glestkeys.ini
 
 
-copy /y vs2015-32\g3d_viewer\Release\g3d_viewer.exe bin\vs2015\g3d_viewer-32.exe
-copy /y vs2017-32\g3d_viewer\Release\g3d_viewer.exe bin\vs2017\g3d_viewer-32.exe
+copy /y vs2015-32\g3d_viewer\g3d_viewer.exe bin\vs2015\g3d_viewer-32.exe
+copy /y vs2017-32\g3d_viewer\g3d_viewer.exe bin\vs2017\g3d_viewer-32.exe
 
-copy /y vs2015-32\map_editor\Release\map_editor.exe bin\vs2015\map_editor-32.exe
-copy /y vs2017-32\map_editor\Release\map_editor.exe bin\vs2017\map_editor-32.exe
+copy /y vs2015-32\map_editor\map_editor.exe bin\vs2015\map_editor-32.exe
+copy /y vs2017-32\map_editor\map_editor.exe bin\vs2017\map_editor-32.exe
 
 copy /y vs2015-32\zetaglest\zetaglest.exe bin\vs2015\zetaglest-32.exe
 copy /y vs2017-32\zetaglest\zetaglest.exe bin\vs2017\zetaglest-32.exe
 
-copy /y vs2015-64\g3d_viewer\Release\g3d_viewer.exe bin\vs2015\g3d_viewer-64.exe
-copy /y vs2017-64\g3d_viewer\Release\g3d_viewer.exe bin\vs2017\g3d_viewer-64.exe
+copy /y vs2015-64\g3d_viewer\g3d_viewer.exe bin\vs2015\g3d_viewer-64.exe
+copy /y vs2017-64\g3d_viewer\g3d_viewer.exe bin\vs2017\g3d_viewer-64.exe
 
-copy /y vs2015-64\map_editor\Release\map_editor.exe bin\vs2015\map_editor-64.exe
-copy /y vs2017-64\map_editor\Release\map_editor.exe bin\vs2017\map_editor-64.exe
+copy /y vs2015-64\map_editor\map_editor.exe bin\vs2015\map_editor-64.exe
+copy /y vs2017-64\map_editor\map_editor.exe bin\vs2017\map_editor-64.exe
 
 copy /y vs2015-64\zetaglest\zetaglest.exe bin\vs2015\zetaglest-64.exe
 copy /y vs2017-64\zetaglest\zetaglest.exe bin\vs2017\zetaglest-64.exe
