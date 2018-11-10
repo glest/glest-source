1347a6ba068aa613a51851e54e79b14e0b54986f
mathusummut <mathusum.mut@gmail.com>
2018-11-10 14:21:25 +0100

    Fixed linux permissions issue

--- mk/linux/build-zg.sh
+++ mk/linux/build-zg.sh
@@ -4,6 +4,10 @@
 # Written by Mark Vejvoda <mark_vejvoda@hotmail.com>
 # Copyright (c) 2011-2013 Mark Vejvoda under GNU GPL v3.0+
 
+currentDir=$PWD
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
 ./build-deps.sh
 echo
 ./build-zg-nodeps.sh
+cd $currentDir
--- mk/linux/setup.sh
+++ mk/linux/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo
 echo "Initializing ZetaGlest setup..."
+currentDir=$PWD
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
 ./clone-data.sh
 echo
 ./pull.sh
 echo
 ./build-zg.sh
+cd $currentDir
