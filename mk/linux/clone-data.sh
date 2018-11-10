23afb587586433fabda4ffcc22960908a61b1a43
mathusummut <mathusum.mut@gmail.com>
2018-11-10 17:26:55 +0100

    Fixed clone release version on Linux

--- mk/linux/clone-data.sh
+++ mk/linux/clone-data.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
 mkdir build
 cd build
-git clone https://github.com/ZetaGlest/zetaglest-data.git data
+git clone https://github.com/ZetaGlest/zetaglest-data.git data --branch 1.0
 cd ..
 
 if [ `id -u`'x' == '0x' ] || [ "$1" == "--manually" ]; then
