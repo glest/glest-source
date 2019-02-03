8ad98398ad9a2220407a40f417904df817d48e60
mathusummut <mathusum.mut@gmail.com>
2019-02-03 14:04:11 +0100

    Fixed setup.sh for mac

--- mk/macos/build-deps.sh
+++ mk/macos/build-deps.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 brew install sdl2 lua fontconfig freetype ftgl libogg glew libvorbis cppunit glib fribidi miniupnpc wxmac
 brew outdated cmake || brew upgrade cmake; brew outdated pkgconfig || brew upgrade pkgconfig
 brew install Caskroom/cask/xquartz
--- mk/macos/setup.sh
+++ mk/macos/setup.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+echo
+echo "Initializing Glest setup..."
+currentDir=$PWD
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
 ./build-deps.sh
+echo
 ./build.sh
+cd $currentDir
