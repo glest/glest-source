249c8a22edaa51f2d7a3a017df9b080090169a69
mathusummut <mathusum.mut@gmail.com>
2019-02-03 14:23:01 +0100

    Fixed setup.sh for mac

--- mk/macos/setup.sh
+++ mk/macos/setup.sh
@@ -4,7 +4,7 @@ echo "Initializing Glest setup..."
 currentDir=$PWD
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
-bash ./build-deps.sh
+sh ./build-deps.sh
 echo
-bash ./build.sh
+sudo sh ./build.sh
 cd $currentDir
