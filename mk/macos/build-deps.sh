3ea4636227b972ab3b03ceb77611b7da445934d4
mathusummut <mathusum.mut@gmail.com>
2019-02-03 14:21:25 +0100

    Fixed setup.sh for mac

--- mk/macos/build-deps.sh
+++ mk/macos/build-deps.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 brew install sdl2 lua fontconfig freetype ftgl libogg glew libvorbis cppunit glib fribidi miniupnpc wxmac
+brew install cmake
 brew outdated cmake || brew upgrade cmake; brew outdated pkgconfig || brew upgrade pkgconfig
 brew install Caskroom/cask/xquartz
