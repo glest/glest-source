918e0a41eea040d2ae010cf53089d23e07ac3eca
mathusummut <mathusum.mut@gmail.com>
2019-04-06 02:17:53 +0200

    Updated version

--- source/shared_lib/include/versions.h
+++ source/shared_lib/include/versions.h
@@ -1,5 +1,5 @@
 #define GAME_VERSION "Glest 2.0"
 //Month then Day
-#define GAME_BUILD_DATE "0321"
+#define GAME_BUILD_DATE "0406"
 #define G3D_VIEWER_VERSION "1.0"
 #define MAP_EDITOR_VERSION "1.0"
